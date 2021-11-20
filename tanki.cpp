feac85e78dc18de5be1b2605293a551119998c49
Gevork Sofoyan <aab8117@gmail.com>
2021-11-20 17:54:29 +0300

    colorfull "Results"

--- tanki.cpp
+++ tanki.cpp
@@ -4,6 +4,27 @@
 #include <ctime>
 
 using namespace std;
+
+#define RESET   "\033[0m"
+
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 //размер консольного поля в символах
 #define width 120
 #define height 24
@@ -418,9 +439,9 @@ int main()
         } while (GetKeyState(VK_ESCAPE) >= 0);//Функция GetKeyState извлекает данные о состоянии заданной виртуальной клавиши, в даном случае при нажатии на Esc игра перезапускается.
 
         cout << endl;
-        cout << "\t\t\t\t\t\t\tResults" << endl;
-        cout << "\t\t\t\t\t\t    Player1 score: " << wins1 << endl;
-        cout << "\t\t\t\t\t\t    Player2 score: " << wins2 << endl << endl;
+        cout << RED << "\t\t\t\t\t\t\tResults" << RESET << endl;
+        cout << BLUE << "\t\t\t\t\t\t    Player1 score: " << RESET << wins1 << endl;
+        cout << GREEN <<"\t\t\t\t\t\t    Robot score\t : "<<RESET << wins2 << endl << endl;
         cout << endl;
     }
 
